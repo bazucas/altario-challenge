e94bc4678e06780a9a341387a567016a68916c02
Luis Inacio <lrs.inacio@gmail.com>
2020-02-16 20:26:12 +0000

    Scss file structure + components generated

--- src/app/@features/generator/generator.module.ts
+++ src/app/@features/generator/generator.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GeneratorComponent } from './generator/generator.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GeneratorComponent],
   imports: [
     CommonModule
   ]
--- src/app/@features/payments/payments.module.ts
+++ src/app/@features/payments/payments.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaymentsComponent } from './payments/payments.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PaymentsComponent],
   imports: [
     CommonModule
   ]
