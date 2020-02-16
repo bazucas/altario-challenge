03eb7f6e1bd3681659c622b32c9a906aa1d0c5ba
Luis Inacio <lrs.inacio@gmail.com>
2020-02-16 20:36:37 +0000

    Just for fun, not-found page with iframe

--- src/app/@features/generator/generator.module.ts
+++ src/app/@features/generator/generator.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GeneratorComponent } from './generator/generator.component';
+import {RouterModule, Routes} from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: GeneratorComponent
+  }
+];
 
 @NgModule({
   declarations: [GeneratorComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class GeneratorModule { }
--- src/app/@features/payments/payments.module.ts
+++ src/app/@features/payments/payments.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentsComponent } from './payments/payments.component';
+import {RouterModule, Routes} from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: PaymentsComponent
+  }
+];
 
 @NgModule({
   declarations: [PaymentsComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class PaymentsModule { }
